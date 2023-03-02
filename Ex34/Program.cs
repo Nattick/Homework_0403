033a27f0bb70cc9d2e5c7dc18984fe0c3972a93d
Nattick <natalia.a.konovalova@gmail.com>
2023-03-02 16:14:29 +0300

    add solution 34

--- Ex34/Program.cs
+++ Ex34/Program.cs
@@ -1,2 +1,7 @@
 ﻿// Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
+int n = Size("Enter array elements number ");
+int [] Print = Array (n);
+int Col = col (Print);
+Console.WriteLine();
+Console.WriteLine ("Even elements number = " + Col);
