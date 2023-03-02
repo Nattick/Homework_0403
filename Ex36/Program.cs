e6cef215a05b058fc6ae22e5f044fe66af95d8dd
Nattick <natalia.a.konovalova@gmail.com>
2023-03-02 16:22:17 +0300

    add solution 36

--- Ex36/Program.cs
+++ Ex36/Program.cs
@@ -1,2 +1,10 @@
-﻿
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+using static Shares;
+int n = Size("Enter array elements number ");
+int [] Print = ArrayRandom (n);
+int summ = sum(Print);
+Console.WriteLine();
+Console.WriteLine(summ);
 
